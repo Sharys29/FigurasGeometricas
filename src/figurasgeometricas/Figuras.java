a64de0f2ac0800328c3c20b078f3790c806a858d
Sharys29 <sharysceciliano@gmail.com>
2026-06-11 13:24:52 -0600

    Perimetro y area
    Se elimina el atributo perimetro y area

--- src/figurasgeometricas/Figuras.java
+++ src/figurasgeometricas/Figuras.java
@@ -9,29 +9,23 @@ package figurasgeometricas;
  * @author sharys
  */
 public abstract class Figuras {
-    protected double area;
-    protected double perimetro;
+    
 
     public double getArea() {
-        return area;
+        return calcularArea();
     }
 
-    public void setArea(double area) {
-        this.area = area;
-    }
-
-    public double getPerimetro() {
-        return perimetro;
-    }
-
-    public void setPerimetro(double perimetro) {
-        this.perimetro = perimetro;
-    }
     
-   public abstract double calcularPerimetro();
+    public double getPerimetro() {
+        return calcularPerimetro();
+    }
+
+   
+    
+   protected abstract double calcularPerimetro();
    //si se pone llaves, genera un error, los metodos abstractos no puede 
    //obtener cuerpo
-   public abstract double calcularArea();
+   protected abstract double calcularArea();
    
     
 }
